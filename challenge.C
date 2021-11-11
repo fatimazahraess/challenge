eab9200d141a55bb7c5da2ae56212c5a61f826bb
fatimazahraess <84686176+fatimazahraess@users.noreply.github.com>
2021-11-11 16:08:28 +0100

    Update challenge.C

--- challenge.C
+++ challenge.C
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char nom;
+    char pren;
+    char age;
+    int sexe;
+    char num;
+    printf("entrer le nom:\n");
+    scanf("%s",&nom);
+    printf("entrer le pren:\n");
+     scanf("%s",&pren);
+      printf("entrer le age:\n");
+     scanf("%s",&age);
+     printf("entrer le sexe:\n");
+     scanf("%s",&sexe);
+    printf("entrer le num:\n");
+        scanf("%s",&num);
 
+
+
+}
